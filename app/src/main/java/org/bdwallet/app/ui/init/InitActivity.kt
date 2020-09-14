3377d0e6249cc6956802a3d4f31d478c2905ba5c
Ethan Smith <ethanasm@usc.edu>
2020-09-14 15:05:58 -0700

    init dialog custom popup completed

--- app/src/main/java/org/bdwallet/app/ui/init/InitActivity.kt
+++ app/src/main/java/org/bdwallet/app/ui/init/InitActivity.kt
@@ -1,12 +1,34 @@
 package org.bdwallet.app.ui.init
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.TextView
+import kotlinx.android.synthetic.main.dialog_init.*
 import org.bdwallet.app.R
 
 class InitActivity : AppCompatActivity() {
+    var dialogHeight: Int = 1000
+    var dialogWidth: Int = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_init)
+        showDialog()
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.dialog_init)
+        val continueText = dialog.findViewById(R.id.dismiss) as TextView
+        continueText.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+        dialog.window!!.setLayout(dialogWidth, dialogHeight)
     }
 }
