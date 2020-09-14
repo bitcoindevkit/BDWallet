1529662be940f89e1a655afc1d6af59928915dcf
Ethan Smith <ethanasm@usc.edu>
2020-09-14 12:42:46 -0700

    colors/themes updated and entry activity flow fixed

--- app/src/main/java/org/bdwallet/app/EntryActivity.kt
+++ app/src/main/java/org/bdwallet/app/EntryActivity.kt
@@ -4,25 +4,25 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class EntryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val pref = getSharedPreferences("init", Context.MODE_PRIVATE)
+        val isWalletInitialized = getSharedPreferences("init", Context.MODE_PRIVATE)
+            .getBoolean("walletInitialized", false)
         val intent = Intent(
             this,
-            //Class.forName(getNextActivityName(pref.getBoolean("walletInitialized", false)))
-            InitActivity::class.java
+            Class.forName(getNextActivityName(isWalletInitialized))
         )
         startActivity(intent)
     }
 
     private fun getNextActivityName(walletInitialized: Boolean): String {
         if (walletInitialized) {
-            return LoginActivity::class.simpleName!!
+            return LoginActivity::class.qualifiedName!!
         }
-        return InitActivity::class.simpleName!!
+        return InitActivity::class.qualifiedName!!
     }
 }
