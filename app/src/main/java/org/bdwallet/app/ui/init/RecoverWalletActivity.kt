0aa92b46e39cc0d152a836043838d9df328a90b8
Ethan Smith <ethanasm@usc.edu>
2020-09-14 17:05:05 -0700

    create and recover activities added and linked to init; init dialog tweaked

---
+++ app/src/main/java/org/bdwallet/app/ui/init/RecoverWalletActivity.kt
@@ -0,0 +1,12 @@
+package org.bdwallet.app.ui.init
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.bdwallet.app.R
+
+class RecoverWalletActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recover_wallet)
+    }
+}
