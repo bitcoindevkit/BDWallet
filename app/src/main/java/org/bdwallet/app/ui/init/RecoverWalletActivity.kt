5538a097735e53d85a0984417ffd28f8da2a08b7
lazertazer <eisnerl@usc.edu>
2020-09-29 16:04:57 -0700

    refactor state management / defaults a bit

--- app/src/main/java/org/bdwallet/app/ui/init/RecoverWalletActivity.kt
+++ app/src/main/java/org/bdwallet/app/ui/init/RecoverWalletActivity.kt
@@ -3,7 +3,6 @@ package org.bdwallet.app.ui.init
 import android.content.Intent
 import android.os.Bundle
 import android.view.Gravity
-import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.Toast
@@ -12,9 +11,6 @@ import org.bdwallet.app.BDWApplication
 import org.bdwallet.app.R
 import org.bdwallet.app.ui.wallet.WalletActivity
 import org.bitcoindevkit.bdkjni.Types.ExtendedKeys
-import org.bitcoindevkit.bdkjni.Types.Network
-import org.bitcoindevkit.bdkjni.Types.WalletPtr
-import java.io.File
 import java.lang.reflect.InvocationTargetException
 
 
@@ -53,15 +49,14 @@ class RecoverWalletActivity : AppCompatActivity() {
         //gather all text in autocompleteview and create mnemonic
 
         var mnemonicString = findViewById<AutoCompleteTextView>(R.id.seed_text_1).text
-        for (x in 1..11){
-            var seedWord =  findViewById<AutoCompleteTextView>(this.viewList[x]).text
+        for (x in 1..11) {
+            val seedWord = findViewById<AutoCompleteTextView>(this.viewList[x]).text
             mnemonicString.append(seedWord)
         }
 
-        try{
-            this.keys = BDWApplication.instance.createExtendedKeys(Network.testnet, mnemonicString.toString())
-
-        } catch (e: InvocationTargetException){
+        try {
+            this.keys = BDWApplication.instance.createExtendedKeys(mnemonicString.toString())
+        } catch (e: InvocationTargetException) {
             this.showInvalidPhraseToast()
             return false
         }
@@ -69,12 +64,12 @@ class RecoverWalletActivity : AppCompatActivity() {
         return true
     }
 
-    private fun loadWallet(){
+    private fun loadWallet() {
         val descriptor = BDWApplication.instance.createDescriptor(this.keys)
         BDWApplication.instance.createWallet(descriptor)
     }
 
-    private fun addAutofill(){
+    private fun addAutofill() {
 
 //        val fileName = "/BIP39/en.txt"
 //        val inputString = application.assets.open(fileName).bufferedReader().use { it.readText() }
@@ -82,8 +77,8 @@ class RecoverWalletActivity : AppCompatActivity() {
 
         this.viewList = listOfNotNull<Int>(R.id.seed_text_1,R.id.seed_text_2, R.id.seed_text_3, R.id.seed_text_4, R.id.seed_text_5, //get AutoCompleteTextViews in list
             R.id.seed_text_6, R.id.seed_text_7, R.id.seed_text_8, R.id.seed_text_9, R.id.seed_text_10, R.id.seed_text_11, R.id.seed_text_12)
-//
-//        for (x in 0..11){ //adding autofill to each AutoCompleteTextview
+
+//        for (x in 0..11) { //adding autofill to each AutoCompleteTextview
 //            var currView = findViewById<AutoCompleteTextView>(this.viewList[x])
 //            val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
 //                this,
@@ -91,7 +86,6 @@ class RecoverWalletActivity : AppCompatActivity() {
 //            )
 //            currView.setAdapter(adapter)
 //        }
-
     }
 
     private fun showInvalidPhraseToast() {
