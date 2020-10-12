182363b56830f0e4cfe70e667ce739c5217ee97d
Steven Marsh <marshs@usc.edu>
2020-10-11 23:06:36 -0700

    settings switch works now
    had to change switchpreferencecompat with a switch and handle it in SettingActivity (not fragment) so need to stylize still

--- app/src/main/java/org/bdwallet/app/ui/wallet/settings/SettingsFragment.kt
+++ app/src/main/java/org/bdwallet/app/ui/wallet/settings/SettingsFragment.kt
@@ -1,39 +1,51 @@
 package org.bdwallet.app.ui.wallet.settings
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import androidx.preference.SwitchPreferenceCompat
 import org.bdwallet.app.BDWApplication
 import org.bdwallet.app.R
+
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        this.checkSwitch()
-    }
-
-    private fun checkSwitch() {
-        var BTCtoSAT =  findPreference<SwitchPreferenceCompat>("sat_convert")
-        if (BTCtoSAT != null) {
-            BTCtoSAT.onPreferenceChangeListener
-        }
-        {
-            if (BTCtoSAT != null) {
-                if (BTCtoSAT.isEnabled){ //switch from BTC to SAT
-
-                } else { //from SAT to BTC
-
-                }
-            }
-        }
-
 
     }
+
+//    private fun checkSwitch() {
+//        var BTCtoSAT =  findPreference<SwitchPreferenceCompat>("sat_convert")
+//       // val walletPreference: SharedPreferences = BDWApplication.instance.getSharedPreferences("saved_wallet", Context.MODE_PRIVATE)
+//        var prefs : SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context)
+//        prefs.get
+//        BTCtoSAT?.onPreferenceChangeListener?.onPreferenceChange(BTCtoSAT, true)
+//
+//        if (BTCtoSAT as Boolean){
+//            BDWApplication.instance.setDenomination("SAT")
+//        } else {
+//            BDWApplication.instance.setDenomination("BTC")
+//        }
+//
+//    }
+
+
+
 
 }
+
+
+
+
+
+
+
