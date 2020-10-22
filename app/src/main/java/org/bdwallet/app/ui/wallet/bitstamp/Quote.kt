2a4a7ba3f1ac2a8ccabf5abc98cfd28e438d012d
Steve Myers <steve@notmandatory.org>
2020-10-22 14:35:27 -0700

    Add Bitstamp TickerService, use in BalanceFragment

---
+++ app/src/main/java/org/bdwallet/app/ui/wallet/bitstamp/Quote.kt
@@ -0,0 +1,3 @@
+package org.bdwallet.app.ui.wallet.bitstamp
+
+class Quote(var high:String , var last: String, var timestamp: String, var bid: String, var vwap: String, var volume: String, var low: String, var ask: String, var open: String)
