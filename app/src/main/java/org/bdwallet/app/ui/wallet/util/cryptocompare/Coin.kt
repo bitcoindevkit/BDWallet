9b66712598815e8aed1ca9a07acf5c7af6a977c1
Richard Myers <8568892+remyers@users.noreply.github.com>
2020-11-27 14:37:37 +0100

    remove unused/unneeded utility code

--- app/src/main/java/org/bdwallet/app/ui/wallet/util/cryptocompare/Coin.kt
+++
@@ -1,5 +0,0 @@
-package org.bdwallet.app.ui.wallet.util.cryptocompare
-
-class Coin(var BTC: String, var ETH: String, var ETC: String, var DASH: String, var MAID: String, var XEM: String, var XRP: String, var AUR: String, var LTC: String, var XMR: String, var USD: String, var EUR: String, var GBP: String) {
-    constructor(): this("", "", "", "", "", "", "", "", "", "", "", "", "")
-}
